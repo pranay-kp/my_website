b241dd8c4fae39893a3762d98f477e0dce6c9f37
pranay <pranaykumarbayya@gmail.com>
2024-03-13 11:35:01 +0530

    2nd update

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from flask_bootstrap import Bootstrap5
 app = Flask(__name__)
 Bootstrap5(app)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+
 
 @app.route("/about")
 def about():
